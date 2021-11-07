1472f77aed0d9b7be7066a049d58d7c75a3ed847
Andre Luis Vasconcelos <alpvj@cin.ufpe.br>
2021-11-07 12:06:39 -0300

    create spring project

---
+++ apspokeshop/src/main/java/aps/pokeshop/apspokeshop/ApspokeshopApplication.java
@@ -0,0 +1,13 @@
+package aps.pokeshop.apspokeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApspokeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApspokeshopApplication.class, args);
+	}
+
+}
---
+++ apspokeshop/src/test/java/aps/pokeshop/apspokeshop/ApspokeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.pokeshop.apspokeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApspokeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
